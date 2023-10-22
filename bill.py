d8b46803b5f745a3ab74423b8f6f0061929c7428
salah_eddine_berret <salaheddineberret@gmail.com>
2023-10-22 14:01:13 +0100

    update

--- bill.py
+++ bill.py
@@ -1,31 +1,39 @@
 # Algorihme Bill
 # variables
-#      b , c , d , e : chaine de caracter 
-#      a , x , y , z : reel
+#      b , c , d , e , l ,m , p : reel  
+#      a , x , y , z : chaine de caracter
+#      n : entier
 # Debut
 #    pour i<--1 a 3 faire
-#    |   Ecrire("enter the name of the customer n'",i+1,":")
+#    |   Ecrire("enter the name of the customer n'",i,":")
 #    |   Lire(a)
-#    |   Ecrire("Give the price of the item :")
-#    |   Lire(b)
-#    |   si i = 1 alors
-#    |   |   c = b + b * 0.15 - b * 0.02 
-#    |   |   x = a
-#    |   sinon
-#    |   |  si i = 2 alors
-#    |   |  |    d = b + b * 0.15 - b * 0.02 
-#    |   |  |    y = a
+#    |   Ecrire("enter the number of items : ")
+#    |   Lire(n)
+#    |   pour j<--1 a n faire
+#    |   |  Ecrire("Give the price of the item n'",j,":")
+#    |   |  Lire(b)
+#    |   |  si i = 1 alors
+#    |   |  |   c = b + b * 0.15 - b * 0.02 
+#    |   |  |   l<-- l + c
+#    |   |  |   x = a
 #    |   |  sinon
-#    |   |  |  si i = 3 alors
-#    |   |  |  |   e = b + b * 0.15 - b * 0.02 
-#    |   |  |  |   z = a
+#    |   |  |  si i = 2 alors
+#    |   |  |  |    d = b + b * 0.15 - b * 0.02
+#    |   |  |  |    m<-- m + d
+#    |   |  |  |    y = a
+#    |   |  |  sinon
+#    |   |  |  |  si i = 3 alors
+#    |   |  |  |  |   e = b + b * 0.15 - b * 0.02
+#    |   |  |  |  |   p<-- p + e 
+#    |   |  |  |  |   z = a
+#    |   |  |  |  Finsi
 #    |   |  |  Finsi
 #    |   |  Finsi
-#    |   Finsi
+#    |   Finpour
 #    Finpour
-#    Ecrire("The Total to be paid for the customer",x,":",c,"DH")
-#    Ecrire("The Total to be paid for the customer",y,":",d,"DH")
-#    Ecrire("The Total to be paid for the customer",z,":",e,"DH")
+#    Ecrire("The Total to be paid for the customer",x,":",l,"DH")
+#    Ecrire("The Total to be paid for the customer",y,":",m,"DH")
+#    Ecrire("The Total to be paid for the customer",z,":",p,"DH")
 # Fin
 
 
